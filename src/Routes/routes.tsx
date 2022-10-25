e6a58ef13629bd95ca4775e5ec4e5d83844536cd
Ali Hassan Kazmi <sahkazmi100@gmail.com>
2022-10-25 12:24:32 +0500

    Added new files to Login Folder

--- src/Routes/routes.tsx
+++ src/Routes/routes.tsx
@@ -1,14 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ProtectedRoute } from "./protectedRoute";
 import Signup from "../App/Screens/Signup/Signup";
-
+import Login from "../App/Screens/Login/Login";
 export const Navigation = () => {
 
   return (
     <Router>
       <Routes>
-        <Route path='/signup' element={<Signup/>} />
-        
+        <Route path='/' element={<Login />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+
+
       </Routes>
     </Router>
   );
