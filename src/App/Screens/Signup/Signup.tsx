0904dac0c488b93eebfe80e9f9433c66022a62bb
John Doe1~ <johndoe@example.com>
2022-10-28 20:21:29 +0500

    URLs set, navigates to feed

--- src/App/Screens/Signup/Signup.tsx
+++ src/App/Screens/Signup/Signup.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import "./Signup.css";
-import { useFormik } from "formik";
+import { useFormik, validateYupSchema } from "formik";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import * as yup from "yup";
+import useAdminSignup from "../../../Hooks/useAdminSignup";
+import { Link, Navigate, useNavigate } from "react-router-dom";
+import CircularProgress from "@mui/material/CircularProgress";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import Location from "./Location";
 
 const validationSchema = yup.object({
-  username: yup.string().required("Username is required"),
+  name: yup.string().required("Username is required"),
   email: yup
     .string()
     .email("Enter a valid email")
@@ -15,21 +21,36 @@ const validationSchema = yup.object({
     .string()
     .min(6, "Password should be of minimum 6 characters length")
     .required("Password is required"),
+  // location: yup.string().required("Location is required"),
 });
 
-const Signup = () => {
+const Signup = (props: any) => {
+  let { userType } = useParams();
+  const [loading, setLoading] = useState(false);
+  const { adminSignup } = useAdminSignup(userType);
   const formik = useFormik({
     initialValues: {
-      username: "",
+      name: "",
       email: "",
       password: "",
+      location: "",
     },
     validationSchema: validationSchema,
     onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
+      adminSignup(
+        values.name,
+        values.email,
+        values.password,
+        values.location,
+        setLoading
+      );
+      setLoading(true);
     },
-  }); 
+  });
 
+  if (userType !== "customer" && userType !== "admin" && userType !== "brand") {
+    return <Navigate to="/404_Not_Found" />;
+  }
   return (
     <Box className="signup">
       <Box className="signupWrapper">
@@ -45,19 +66,26 @@ const Signup = () => {
               }}
             >
               <form onSubmit={formik.handleSubmit}>
+                {userType === "admin" ? (
+                  <h1 className="signHeading">Signup as an Admin</h1>
+                ) : userType === "brand" ? (
+                  <h1 className="signHeading">Signup as a Brand</h1>
+                ) : (
+                  <h1 className="signHeading">Signup as a Customer</h1>
+                )}
+
                 <TextField
                   fullWidth
-                  id="username"
-                  name="username"
+                  id="name"
+                  name="name"
                   label="Username"
-                  value={formik.values.username}
+                  value={formik.values.name}
                   onChange={formik.handleChange}
-                  error={
-                    formik.touched.username && Boolean(formik.errors.username)
-                  }
-                  helperText={formik.touched.username && formik.errors.username}
+                  error={formik.touched.name && Boolean(formik.errors.name)}
+                  helperText={formik.touched.name && formik.errors.name}
                   onBlur={formik.handleBlur}
                 />
+
                 <TextField
                   fullWidth
                   id="email"
@@ -82,7 +110,35 @@ const Signup = () => {
                   helperText={formik.touched.password && formik.errors.password}
                   onBlur={formik.handleBlur}
                 />
-                <button className="signButton">Submit</button>
+                {userType === "brand" && (
+                  <Location
+                    id="location"
+                    value={formik.values.location}
+                    onChange={formik.handleChange}
+                  />
+                )}
+
+                {loading && (
+                  <Box
+                    sx={{
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      marginLeft: "-6rem",
+                    }}
+                  >
+                    <CircularProgress />
+                  </Box>
+                )}
+                <button className="signButton" type="submit">
+                  SIGNUP
+                </button>
+
+                <span id="span">Already have an account?</span>
+                <br></br>
+                <Link id="span2" to="/">
+                  LOGIN
+                </Link>
               </form>
             </Box>
           </Box>
--- src/Routes/routes.tsx
+++ src/Routes/routes.tsx
@@ -3,19 +3,15 @@ import { ProtectedRoute } from "./protectedRoute";
 import Signup from "../App/Screens/Signup/Signup";
 import Login from "../App/Screens/Login/Login";
 export const Navigation = () => {
-
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Login />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/signup' element={<Signup />} />
-
-
+        <Route path="/" element={<Login />} />
+        <Route path="/:userType" element={<Signup />} />
+        <Route path="/:userType" element={<Signup />} />
+        <Route path="/:userType" element={<Signup />} />
+        <Route path='/404_Not_Found' element={<>Page not found</>} />
       </Routes>
     </Router>
   );
-
-}
-
-
+};
