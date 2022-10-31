6148271df3ae916ab4c4687b2407d24c760af8a6
abdstacker <muhammadabdullahaftab@gmail.com>
2022-10-31 16:10:29 +0500

    modified

--- src/Hooks/usePostChallenge.ts
+++ src/Hooks/usePostChallenge.ts
@@ -7,21 +7,18 @@ const usePostChallenge = () => {
     challengeImage: Blob,
     challengeTag: string
   ) => {
-    // const token =
-    //   "eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MiwiZXhwIjoxNjcyMDYxOTMxLCJ2YWwiOjJ9.a-EhTSigS3Kw-hrZ5mCcrbzDGOXHTwc6qL_Z99wqdl0";
-    console.log("in hook", challengeImage);
+    const token = window.localStorage.getItem("token");
     const data = new FormData();
     data.append("challenge[title]", challengeName);
     data.append("challenge[description]", challengeDescription);
-    data.append("tag[id]", challengeTag);
+    data.append("tag[title]", challengeTag);
     data.append("challenge[images][]", challengeImage);
-    console.log("in formdata", data);
+
     var config = {
       method: "post",
       url: "https://project2-p2.herokuapp.com/api/challenges",
       headers: {
-        Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJpZCI6NTIsImV4cCI6MTY3MjM4MjI3NSwidmFsIjoyfQ.ZrZMyBzSZBIAr_cHmqdpOsMSIgDY0GXhQMfGpMGwqEI",
+        Authorization: `Bearer ${token}`,
       },
       data: data,
     };
